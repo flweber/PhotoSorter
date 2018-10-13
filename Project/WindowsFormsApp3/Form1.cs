8a66fa7b9b2ca340ce9fd928c26ca69e8fa2e062
Florian Weber <F.Weber@SOLARLUX.DE>
2018-10-13 07:41:48 +0200

    Initialize Commit

---
+++ Project/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Danketext = "Danke für deine Eingabe";
+            Danketext = Danketext.Replace(' ', '1');
+            DialogResult Auswahl = MessageBox.Show(Danketext, "Danke", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+            if (Auswahl == DialogResult.OK)
+            {
+                Close();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
